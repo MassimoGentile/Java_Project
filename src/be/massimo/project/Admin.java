3b2f37dcda5e618027c1edea91fd420ad37dcf97
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 16:11:45 +0100

    Setting Up Admin Class

--- src/be/massimo/project/Admin.java
+++ src/be/massimo/project/Admin.java
@@ -1,5 +1,11 @@
 package be.massimo.project;
 
-public class Admin {
+import java.util.Date;
 
+public class Admin extends User{
+	
+	public Admin(int id, String name, String firstname, Date birthday, String email, String password, String address) {
+		super(id, name, firstname, birthday, email, password, address);
+	}
+	
 }
