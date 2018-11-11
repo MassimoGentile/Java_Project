9007e701519940aae2ae8cef4138e058ea489963
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 16:48:53 +0100

    Add in Loan Class
    Add of the set function and a new attribute

--- src/be/massimo/project/Loan.java
+++ src/be/massimo/project/Loan.java
@@ -1,40 +1,72 @@
 package be.massimo.project;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Loan {
 	
+	//	ATTRIBUTES
 	private int Id;
-	private Date DateDebut;
-	private Date DateFin;
+	private LocalDateTime BeginDate;
+	private LocalDateTime EndDate;
 	private Borrower B;
 	private Lender L;
+	private Copy C;
 	
-	public Loan(int id, Date datedebut, Date datefin, Borrower b, Lender l) {
-		this.Id = id;
-		this.DateDebut = datedebut;
-		this.DateFin = datefin;
-		this.B = b;
-		this.L = l;
-	}
-	
+	//	GET/SET
 	public int getId() {
 		return this.Id;
 	}
 	
-	public Date getDateDebut() {
-		return this.DateDebut;
+	public void setId(int id) {
+		this.Id = id;
 	}
 	
-	public Date getDateFin() {
-		return this.DateFin;
+	public LocalDateTime getBeginDate() {
+		return this.BeginDate;
+	}
+	
+	public void setBeginDate(LocalDateTime beginDate) {
+		this.BeginDate = beginDate;
+	}
+	
+	public LocalDateTime getEndDate() {
+		return this.EndDate;
+	}
+	
+	public void setEndDate(LocalDateTime endDate) {
+		this.EndDate = endDate;
 	}
 	
 	public Borrower getBorrower() {
 		return this.B;
 	}
 	
+	public void setBorrower(Borrower b) {
+		this.B = b;
+	}
+	
 	public Lender getLender() {
 		return this.L;
+	}
+	
+	public void setLender(Lender l) {
+		this.L = l;
+	}
+	
+	public Copy getCopy() {
+		return this.C;
+	}
+	
+	public void setCopy(Copy c) {
+		this.C = c;
+	}
+	
+	// 	CONSTRUCTOR(S)
+	public Loan(int id, LocalDateTime datedebut, LocalDateTime datefin, Borrower b, Lender l) {
+		this.Id = id;
+		this.BeginDate = datedebut;
+		this.EndDate = datefin;
+		this.B = b;
+		this.L = l;
 	}
 }
