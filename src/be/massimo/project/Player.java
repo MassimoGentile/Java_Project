cf8b3b0c87ad9e951ccdc721404bb59183ed2f68
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 16:39:23 +0100

    Add in Player Class
    Add the set function and a new constructor

--- src/be/massimo/project/Player.java
+++ src/be/massimo/project/Player.java
@@ -1,24 +1,37 @@
 package be.massimo.project;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Player extends User{
 	
+	// 	ATTRIBUTES
 	private int Amount;
-	private Date RegisterDate;
+	private LocalDateTime RegisterDate;
 	
-	public Player(int id, String name, String firstname, Date birthday, String email, String password, String address, int amount, Date registerDate) {
-		super(id, name, firstname, birthday, email, password, address);
-		this.Amount = amount;
-		this.RegisterDate = registerDate;
-	}
-	
+	// 	GET/SET
 	public int getAmount() {
 		return this.Amount;
 	}
 	
-	public Date getRegisterDate() {
+	public void setAmount(int amount) {
+		this.Amount = amount;
+	}
+	
+	public LocalDateTime getRegisterDate() {
 		return this.RegisterDate;
 	}
 	
+	//	CONSTRUCTOR(S)
+	public Player(int id, String name, String firstname, Date birthday, String email, String password, String address, LocalDateTime registerDate) {
+		super(id, name, firstname, birthday, email, password, address);
+		this.Amount = 10;
+		this.RegisterDate = registerDate;
+	}
+	
+	public Player(int id, String email, String password) {
+		super(id, email, password);
+		this.Amount = 10;
+		this.RegisterDate = LocalDateTime.now();
+	}
 }
