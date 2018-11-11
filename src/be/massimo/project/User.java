e10c94a5d61fd224043c365bbd9a7efa86733b2b
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 16:27:55 +0100

    Add In User Class
    Add the set function  and a new constructor

--- src/be/massimo/project/User.java
+++ src/be/massimo/project/User.java
@@ -3,6 +3,8 @@ package be.massimo.project;
 import java.util.Date;
 
 public abstract class User {
+	
+	//	ATTRIBUTES
 	private int Id;
 	private String Name;
 	private String Firstname;
@@ -11,6 +13,64 @@ public abstract class User {
 	private String Password;
 	private String Address;
 	
+	//	GET/SET
+	public int getId() {
+		return this.Id;
+	}
+	
+	public void setId(int id) {
+		this.Id = id;
+	}
+	
+	public String getName() {
+		return this.Name;
+	}
+	
+	public void setName(String name) {
+		this.Name = name;
+	}
+	
+	public String getFirstname() {
+		return this.Firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.Firstname = firstname;
+	}
+	
+	public Date getBirthday() {
+		return this.Birthday;
+	}
+	
+	public void setBirthday(Date birthday) {
+		this.Birthday = birthday;
+	}
+	
+	public String getEmail() {
+		return this.Email;
+	}
+	
+	public void setEmail(String email) {
+		this.Email = email;
+	}
+	
+	public String getPassword() {
+		return this.Password;
+	}
+	
+	public void setPassword(String password) {
+		this.Password = password;
+	}
+	
+	public String getAddress() {
+		return this.Address;
+	}
+	
+	public void setAddress(String address) {
+		this.Address = address;
+	}
+	
+	//	CONSTRUCTOR(S)
 	public User(int id, String name, String firstname, Date birthday, String email, String password, String address) {
 		this.Id = id;
 		this.Name = name;
@@ -21,31 +81,13 @@ public abstract class User {
 		this.Address = address;
 	}
 	
-	public int getId() {
-		return this.Id;
-	}
-	
-	public String getName() {
-		return this.Name;
-	}
-	
-	public String getFirstname() {
-		return this.Firstname;
-	}
-	
-	public Date getBirthday() {
-		return this.Birthday;
-	}
-	
-	public String getEmail() {
-		return this.Email;
-	}
-	
-	public String getPassword() {
-		return this.Password;
-	}
-	
-	public String getAddress() {
-		return this.Address;
+	public User(int id, String email, String password) {
+		this.Id = id;
+		this.Name = "";
+		this.Firstname = "";
+		this.Birthday = new Date();
+		this.Email = email;
+		this.Password = password;
+		this.Address = "";
 	}
 }
