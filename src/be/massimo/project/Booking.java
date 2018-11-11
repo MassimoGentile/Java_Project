4c88579f15b74bdd6003a936316f6666348854ed
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 17:26:07 +0100

    Setting Up Booking Class

--- src/be/massimo/project/Booking.java
+++ src/be/massimo/project/Booking.java
@@ -1,5 +1,36 @@
 package be.massimo.project;
 
-public class Booking {
+import java.time.LocalDateTime;
 
+public class Booking {
+	//	ATTRIBUTES
+		private LocalDateTime BeginDateWanted;
+		private Game GameWanted;
+		private Borrower B;
+		private LocalDateTime BookingDate;
+		
+	//	GET/SET
+		public LocalDateTime getBeginDateWanted() {
+			return this.BeginDateWanted;
+		}
+		
+		public Game getGameWanted() {
+			return this.GameWanted;
+		}
+		
+		public Borrower getBorrower() {
+			return this.B;
+		}
+		
+		public LocalDateTime getBookingDate() {
+			return this.BookingDate;
+		}
+		
+	//	CONSTRUCTOR(S)
+		public Booking(LocalDateTime beginDateWanted, Game gameWanted, Borrower borrower, LocalDateTime bookingDate) {
+			this.BeginDateWanted = beginDateWanted;
+			this.GameWanted = gameWanted;
+			this.B = borrower;
+			this.BookingDate = bookingDate;
+		}
 }
