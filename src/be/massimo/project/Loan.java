6bd135c2e3edf1c0991fede59090fc6d7fdb5ce8
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 16:07:51 +0100

    Setting Up Loan Class

--- src/be/massimo/project/Loan.java
+++ src/be/massimo/project/Loan.java
@@ -1,5 +1,40 @@
 package be.massimo.project;
 
-public class Loan {
+import java.util.Date;
 
+public class Loan {
+	
+	private int Id;
+	private Date DateDebut;
+	private Date DateFin;
+	private Borrower B;
+	private Lender L;
+	
+	public Loan(int id, Date datedebut, Date datefin, Borrower b, Lender l) {
+		this.Id = id;
+		this.DateDebut = datedebut;
+		this.DateFin = datefin;
+		this.B = b;
+		this.L = l;
+	}
+	
+	public int getId() {
+		return this.Id;
+	}
+	
+	public Date getDateDebut() {
+		return this.DateDebut;
+	}
+	
+	public Date getDateFin() {
+		return this.DateFin;
+	}
+	
+	public Borrower getBorrower() {
+		return this.B;
+	}
+	
+	public Lender getLender() {
+		return this.L;
+	}
 }
