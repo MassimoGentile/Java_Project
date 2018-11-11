0666ecff2317f2a95c0ca0ba13c99c191bee3834
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 15:59:54 +0100

    Setting Up Game Class

--- src/be/massimo/project/Game.java
+++ src/be/massimo/project/Game.java
@@ -1,5 +1,38 @@
 package be.massimo.project;
 
 public class Game {
-
+	
+	private int Id;
+	private String Name;
+	private String Developers;
+	private String Editor;
+	private int Unit;
+	
+	public Game(int id,String name, String developers, String editor, int unit) {
+		this.Id = id;
+		this.Name = name;
+		this.Developers = developers;
+		this.Editor = editor;
+		this.Unit = unit;
+	}
+	
+	public int getId() {
+		return this.Id;
+	}
+	
+	public String getName() {
+		return this.Name;
+	}
+	
+	public String getDevelopers() {
+		return this.Developers;
+	}
+	
+	public String getEditor() {
+		return this.Editor;
+	}
+	
+	public int getUnit() {
+		return this.Unit;
+	}
 }
