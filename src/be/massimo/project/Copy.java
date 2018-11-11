61fda88b9a32d7c3a9668a607483a3d9705710f0
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-11 17:11:35 +0100

    Setting Up Copy Class

--- src/be/massimo/project/Copy.java
+++ src/be/massimo/project/Copy.java
@@ -1,5 +1,43 @@
 package be.massimo.project;
 
-public class Copy {
+import java.time.LocalDateTime;
 
+public class Copy {
+	
+	//	Attributes
+	private Game G;
+	private LocalDateTime DateAdded;
+	private Lender L;
+	
+	//	GET/SET
+	public Game getGame() {
+		return this.G;
+	}
+	
+	public void setGame(Game g) {
+		this.G = g;
+	}
+	
+	public LocalDateTime getDateAdded() {
+		return this.DateAdded;
+	}
+	
+	public void setDateAdded(LocalDateTime dateAdded) {
+		this.DateAdded = dateAdded;
+	}
+	
+	public Lender getLender() {
+		return this.L;
+	}
+	
+	public void setLender(Lender l) {
+		this.L = l;
+	}
+	
+	//	CONSTRUCTOR(S)
+	public Copy(Game game, Lender lender) {
+		this.G = game;
+		this.DateAdded = LocalDateTime.now();
+		this.L = lender;
+	}
 }
