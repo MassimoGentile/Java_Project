497685ec7cdcb05cf744726e1c50dd9da10f495e
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-12 13:09:32 +0100

    Setting Up Console Class

--- src/be/massimo/project/Console.java
+++ src/be/massimo/project/Console.java
@@ -1,5 +1,31 @@
 package be.massimo.project;
 
 public class Console {
-
+	
+	// 	ATTRIBUTES
+	private String Name;
+	private String Version;
+	
+	// 	GET/SET
+	public String getName() {
+		return this.Name;
+	}
+	
+	public void setName(String name) {
+		this.Name = name;
+	}
+	
+	public String getVersion() {
+		return this.Version;
+	}
+	
+	public void setVersion(String version) {
+		this.Version = version;
+	}
+	
+	//	CONSTRUCTOR(S)
+	public Console(String name, String version) {
+		this.Name = name;
+		this.Version = version;
+	}
 }
