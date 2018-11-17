58adf7a1a834d1f5668d887da2cf307bc157573b
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-17 10:27:17 +0100

    Removing Absract Attributes for Player Class

--- src/be/massimo/pojo/Player.java
+++ src/be/massimo/pojo/Player.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public abstract class Player extends User{
+public class Player extends User{
 	
 	// 	ATTRIBUTES
 	private int Amount;
