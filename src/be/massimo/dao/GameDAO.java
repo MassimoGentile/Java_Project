0fdf246ca254fc8a11946800aae9c7c1f60e3961
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-17 18:20:58 +0100

    Setting Up the GameDAO pattern

--- src/be/massimo/dao/GameDAO.java
+++ src/be/massimo/dao/GameDAO.java
@@ -1,5 +1,32 @@
 package be.massimo.dao;
 
-public class GameDAO {
+import java.sql.Connection;
 
+import be.massimo.pojo.Game;
+
+public class GameDAO extends DAO<Game>{
+	
+	public GameDAO(Connection conn) {
+		super(conn);
+	}
+	
+	@Override
+	public boolean create(Game obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean delete(Game obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean update(Game obj) {
+		return false;
+	}
+	
+	@Override
+	public Game find(int id) {
+		return null;
+	}
 }
