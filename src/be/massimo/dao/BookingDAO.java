6ea0ca53d4ef61c0f53ef8d14562fc64bf68a9ea
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-17 18:22:51 +0100

    Setting Up BookingDAO Pattern

--- src/be/massimo/dao/BookingDAO.java
+++ src/be/massimo/dao/BookingDAO.java
@@ -1,5 +1,32 @@
 package be.massimo.dao;
 
-public class BookingDAO {
+import java.sql.Connection;
 
+import be.massimo.pojo.Booking;
+
+public class BookingDAO extends DAO<Booking>{
+	
+	public BookingDAO(Connection conn) {
+		super(conn);
+	}
+	
+	@Override
+	public boolean create(Booking obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean delete(Booking obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean update(Booking obj) {
+		return false;
+	}
+	
+	@Override
+	public Booking find(int id) {
+		return null;
+	}
 }
