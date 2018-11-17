3c9f22a54f8315502e05f09a8ec2a922bc9b55c1
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-17 18:24:03 +0100

    Setting Up ConsoleDAO Pattern

--- src/be/massimo/dao/ConsoleDAO.java
+++ src/be/massimo/dao/ConsoleDAO.java
@@ -1,5 +1,33 @@
 package be.massimo.dao;
 
-public class ConsoleDAO {
+import java.sql.Connection;
 
+import be.massimo.pojo.Console;
+
+
+public class ConsoleDAO extends DAO<Console>{
+	
+	public ConsoleDAO(Connection conn) {
+		super(conn);
+	}
+	
+	@Override
+	public boolean create(Console obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean delete(Console obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean update(Console obj) {
+		return false;
+	}
+	
+	@Override
+	public Console find(int id) {
+		return null;
+	}
 }
