da8054fce1c4c0d3faf3fcb2b3325c223c362b2e
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-17 18:25:13 +0100

    Setting Up CopyDAO Pattern

--- src/be/massimo/dao/CopyDAO.java
+++ src/be/massimo/dao/CopyDAO.java
@@ -1,5 +1,33 @@
 package be.massimo.dao;
 
-public class CopyDAO {
+import java.sql.Connection;
 
+import be.massimo.pojo.Copy;
+
+
+public class CopyDAO extends DAO<Copy>{
+
+	public CopyDAO(Connection conn) {
+		super(conn);
+	}
+	
+	@Override
+	public boolean create(Copy obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean delete(Copy obj) {
+		return false;
+	}
+	
+	@Override
+	public boolean update(Copy obj) {
+		return false;
+	}
+	
+	@Override
+	public Copy find(int id) {
+		return null;
+	}
 }
