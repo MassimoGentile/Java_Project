b411df7e63a5f4dade4063abd7eff7f1a5840363
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-18 16:55:11 +0100

    Update PlayerDAO.java
    Adding the Create Function

--- src/be/massimo/dao/PlayerDAO.java
+++ src/be/massimo/dao/PlayerDAO.java
@@ -1,6 +1,7 @@
 package be.massimo.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 import be.massimo.pojo.Player;
 
@@ -13,7 +14,15 @@ public class PlayerDAO extends DAO<Player>{
 	
 	@Override
 	public boolean create(Player obj) {
-		return false;
+		try {
+			this.Connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("INSERT INTO User (User_Name, User_Firstname, User_Birthday, User_Address, User_Email, User_Password, User_Admin, User_Amount, User_RegisterDate) VALUES (" + obj.getName() + "," + obj.getFirstname() + "," + obj.getBirthday() + "," + obj.getAddress() + "," + obj.getEmail() + "," + obj.getPassword() + "," + obj.getAdmin() + "," + obj.getAmount() + "," + obj.getRegisterDate() + ")");
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	@Override
