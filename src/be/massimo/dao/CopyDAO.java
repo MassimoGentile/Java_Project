53c3a77d51a13d4992a3427f6ce9204e136dacf6
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-18 15:23:38 +0100

    Update CopyDAO.java
    Adding the Insert Function

--- src/be/massimo/dao/CopyDAO.java
+++ src/be/massimo/dao/CopyDAO.java
@@ -1,6 +1,7 @@
 package be.massimo.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 import be.massimo.pojo.Copy;
 
@@ -13,7 +14,15 @@ public class CopyDAO extends DAO<Copy>{
 	
 	@Override
 	public boolean create(Copy obj) {
-		return false;
+		try {
+			this.Connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("INSERT INTO Copy (Copy_DateAdded, Game_Id) VALUES (" + obj.getDateAdded() + "," + obj.getGame().getId() + ")");
+		}catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	@Override
