42fc8554401960d4aaff67a340e68bb47dd8101e
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-22 11:00:15 +0100

    Update ConsoleDAO.java
    Altering SQL request by adding simple quote.

--- src/be/massimo/dao/ConsoleDAO.java
+++ src/be/massimo/dao/ConsoleDAO.java
@@ -18,7 +18,7 @@ public class ConsoleDAO extends DAO<Console>{
 		try {
 			this.Connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY).executeUpdate("INSERT INTO Console (CName, CVersion) VALUES (" + obj.getName() + "," + obj.getVersion() + ")");
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("INSERT INTO Console (CName, CVersion) VALUES ( '" + obj.getName() + "', '" + obj.getVersion() + "')");
 		}catch(SQLException e) {
 			e.printStackTrace();
 			return false;
@@ -44,7 +44,7 @@ public class ConsoleDAO extends DAO<Console>{
 		try {
 			this.Connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY).executeUpdate("UPDATE Console SET CName =" + obj.getName() + ", CVersion =" + obj.getVersion() + " WHERE Console_Id =" + obj.getId());
+					ResultSet.CONCUR_READ_ONLY).executeUpdate("UPDATE Console SET CName = '" + obj.getName() + "', CVersion = '" + obj.getVersion() + "' WHERE Console_Id =" + obj.getId());
 		}catch(SQLException e) {
 			e.printStackTrace();
 			return false;
@@ -58,7 +58,7 @@ public class ConsoleDAO extends DAO<Console>{
 		try {
 			ResultSet result = this.Connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Console WHERE Console_Id =" + id);
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT * FROM Console WHERE Console_Id = " + id);
 			if(result.first())
 				console = new Console(id, result.getString("CName"), result.getString("CVersion"));
 		}catch(SQLException e) {
