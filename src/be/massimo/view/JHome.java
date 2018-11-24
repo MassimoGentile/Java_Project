8ca7fa6b163d60ace501afde7c72568095aa0918
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-24 19:44:41 +0100

    Add Disconnect and Settings
    Add buttons with Icons and implementation of the disconnect button.

--- src/be/massimo/view/JHome.java
+++ src/be/massimo/view/JHome.java
@@ -8,6 +8,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
 
 public class JHome extends JFrame {
 
@@ -35,20 +42,48 @@ public class JHome extends JFrame {
 	public JHome() {
 		setTitle("Java_Project - Home");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 280);
+		setBounds(100, 100, 722, 569);
 		contentPane = new JPanel();
+		contentPane.setOpaque(false);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnBorrow = new JButton("Borrow");
 		btnBorrow.setFont(new Font("Tahoma", Font.BOLD, 35));
-		btnBorrow.setBounds(10, 78, 195, 77);
+		btnBorrow.setBounds(39, 403, 195, 77);
 		contentPane.add(btnBorrow);
 		
 		JButton btnLend = new JButton("Lend");
 		btnLend.setFont(new Font("Tahoma", Font.BOLD, 35));
-		btnLend.setBounds(229, 78, 195, 77);
+		btnLend.setBounds(487, 403, 195, 77);
 		contentPane.add(btnLend);
+		
+		JButton btnProfil = new JButton("");
+		btnProfil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Redirect to JAlterProfil by clicking on the gearing
+			}
+		});
+		btnProfil.setContentAreaFilled(false);
+		btnProfil.setBorderPainted(false);
+		btnProfil.setBackground(SystemColor.menu);
+		btnProfil.setIcon(new ImageIcon(JHome.class.getResource("/Images/settings32.png")));
+		btnProfil.setBounds(52, 11, 32, 32);
+		contentPane.add(btnProfil);
+		
+		JButton btnDisconnect = new JButton("");
+		btnDisconnect.setBorderPainted(false);
+		btnDisconnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JLogin login = new JLogin();
+				login.setVisible(true);
+				dispose();
+			}
+		});
+		btnDisconnect.setContentAreaFilled(false);
+		btnDisconnect.setIcon(new ImageIcon(JHome.class.getResource("/Images/logout-sign32.png")));
+		btnDisconnect.setBounds(10, 11, 32, 32);
+		contentPane.add(btnDisconnect);
 	}
 }
