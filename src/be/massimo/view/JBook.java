4fcb87e33426d192ccd3348ed2bd815d07cd4d0e
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-25 15:39:03 +0100

    Create JBook.java
    Implementation of all the visual content + implementaition of the navigation between JBook and JBorrow for the Back and Book button + Implementation of the navigation between JLogin and JBook for the disconnect button.

---
+++ src/be/massimo/view/JBook.java
@@ -0,0 +1,111 @@
+package be.massimo.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JBook extends JFrame {
+
+	private JPanel contentPane;
+	private JTable tableGameBook;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JBook frame = new JBook();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JBook() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 714, 337);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnDisconnect = new JButton("");
+		btnDisconnect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JHome home = new JHome();
+				home.setVisible(true);
+				dispose();
+			}
+		});
+		btnDisconnect.setContentAreaFilled(false);
+		btnDisconnect.setBorderPainted(false);
+		btnDisconnect.setIcon(new ImageIcon(JBook.class.getResource("/Images/logout-sign32.png")));
+		btnDisconnect.setBounds(10, 11, 32, 32);
+		contentPane.add(btnDisconnect);
+		
+		JLabel lblNewLabel = new JLabel("Unit Amount :");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(516, 11, 106, 32);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblUnit = new JLabel("10");
+		lblUnit.setHorizontalAlignment(SwingConstants.LEFT);
+		lblUnit.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblUnit.setBounds(628, 11, 50, 32);
+		contentPane.add(lblUnit);
+		
+		tableGameBook = new JTable();
+		tableGameBook.setBorder(new LineBorder(new Color(0, 0, 0)));
+		tableGameBook.setBounds(39, 128, 644, 122);
+		contentPane.add(tableGameBook);
+		
+		JLabel lblNewLabel_2 = new JLabel("Informations about the game");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblNewLabel_2.setBounds(39, 97, 644, 23);
+		contentPane.add(lblNewLabel_2);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JBorrow borrow = new JBorrow();
+				borrow.setVisible(true);
+				dispose();
+			}
+		});
+		btnBack.setBounds(39, 261, 89, 23);
+		contentPane.add(btnBack);
+		
+		JButton btnBookThisGame = new JButton("Book this game");
+		btnBookThisGame.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//Implementation require for the booking function
+				JHome home = new JHome();
+				home.setVisible(true);
+				dispose();
+			}
+		});
+		btnBookThisGame.setBounds(553, 261, 125, 23);
+		contentPane.add(btnBookThisGame);
+	}
+}
