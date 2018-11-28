94d6ed8ebc875e2feb690820bf181ecfbfbcd893
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-28 02:37:56 +0100

    Update CopyBusiness.java
    Add find Game and find Console function

--- src/be/massimo/BusinessLayer/CopyBusiness.java
+++ src/be/massimo/BusinessLayer/CopyBusiness.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.util.List;
 
 import be.massimo.dao.ConnectionAccess;
+import be.massimo.dao.ConsoleDAO;
 import be.massimo.dao.CopyDAO;
+import be.massimo.dao.GameDAO;
+import be.massimo.pojo.Console;
 import be.massimo.pojo.Copy;
 import be.massimo.pojo.Game;
 import be.massimo.pojo.Player;
@@ -30,5 +33,13 @@ public class CopyBusiness {
 				return false;
 		}else
 			return false;
+	}
+	
+	public Game findGame(int id) {
+		return new GameDAO(conn).find(id);
+	}
+	
+	public Console findConsole(int id) {
+		return new ConsoleDAO(conn).find(id);
 	}
 }
