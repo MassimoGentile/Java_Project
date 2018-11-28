6937c88d4fba514220570d09f36f53170198c052
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-28 01:56:15 +0100

    Update CopyBusiness.java
    Implementation of an Add function.

--- src/be/massimo/BusinessLayer/CopyBusiness.java
+++ src/be/massimo/BusinessLayer/CopyBusiness.java
@@ -6,6 +6,8 @@ import java.util.List;
 import be.massimo.dao.ConnectionAccess;
 import be.massimo.dao.CopyDAO;
 import be.massimo.pojo.Copy;
+import be.massimo.pojo.Game;
+import be.massimo.pojo.Player;
 
 public class CopyBusiness {
 	
@@ -17,5 +19,16 @@ public class CopyBusiness {
 	
 	public List<Copy> getOwnCopy(int id){
 		return new CopyDAO(conn).getOwnCopy(id);
+	}
+	
+	public boolean Add(Game game, Player player) {
+		if(game != null && player != null) {
+			Copy copy = new Copy(game,player);
+			if(new CopyDAO(conn).create(copy))
+				return true;
+			else 
+				return false;
+		}else
+			return false;
 	}
 }
