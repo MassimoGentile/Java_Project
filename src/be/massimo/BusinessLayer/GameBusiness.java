eaf58df878c8bc47d684a824b2c52fb003b7ce16
Gentile Massimo <mbg.gentile_massimo@hotmail.fr>
2018-11-29 22:54:59 +0100

    Update GameBusiness.java
    Add Modification Function

--- src/be/massimo/BusinessLayer/GameBusiness.java
+++ src/be/massimo/BusinessLayer/GameBusiness.java
@@ -3,8 +3,11 @@ package be.massimo.BusinessLayer;
 import java.sql.Connection;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import be.massimo.dao.ConnectionAccess;
 import be.massimo.dao.GameDAO;
+import be.massimo.pojo.Console;
 import be.massimo.pojo.Game;
 
 public class GameBusiness {
@@ -17,5 +20,13 @@ public class GameBusiness {
 	
 	public List<Game> getAll(){
 		return new GameDAO(conn).getAll();
+	}
+	
+	public void Modification(int id, String name, int releaseYear, String editor, int unit, Console console) {
+		if(id > 0 && name != null && releaseYear != 0 && editor != null && unit != 0 && console != null) {
+			GameDAO gameDAO = new GameDAO(conn);
+			gameDAO.update(new Game(id, name, releaseYear, editor, unit, console));
+			JOptionPane.showMessageDialog(null, "Game Updated !", "Success", JOptionPane.INFORMATION_MESSAGE);
+		}	
 	}
 }
